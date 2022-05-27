d6635b081da255afc273c436589722ee8ddc524c
German Hornus <hornusgerman@gmail.com>
2022-05-27 09:52:26 -0300

    resuelto

--- supermarket-list/case-0/src/App.tsx
+++ supermarket-list/case-0/src/App.tsx
@@ -1,28 +1,33 @@
-import type {Item} from "./types";
-
-import {useEffect, useState} from "react";
-
-import styles from "./App.module.scss";
+import { useEffect, useState } from "react";
 import api from "./api";
+import styles from "./App.module.scss";
+import type { Item } from "./types";
+
+
 
 function App() {
   const [items, setItems] = useState<Item[] | null>(null);
 
   useEffect(() => {
     api.list().then(setItems);
+    document.getElementById("input").focus()
   }, []);
+
+  function handleDelete(id: number) {
+    setItems(items?.filter((item) => item.id !== id));
+  }
 
   return (
     <main className={styles.main}>
       <h1>Supermarket list</h1>
       <form>
-        <input name="text" type="text" />
+        <input name="text" type="text" id="input"/>
         <button>Add</button>
       </form>
       <ul>
         {items?.map((item) => (
           <li key={item.id} className={item.completed ? styles.completed : ""}>
-            {item.text} <button>[X]</button>
+            {item.text} <button onClick={() => handleDelete(item.id)}>[X]</button>
           </li>
         ))}
       </ul>
